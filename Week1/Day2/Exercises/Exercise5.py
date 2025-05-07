e35c521db4f04418d3beeba7384b4b7e1e1bad53
KenSaih <saihkanza63@gmail.com>
2025-05-07 22:18:00 +0100

    random

---
+++ Week1/Day2/Exercises/Exercise5.py
@@ -0,0 +1,15 @@
+import random
+
+def campare_numbers(user_number):
+    if not 1 <= user_number <= 100:
+        return "Please enter a number between 1 and 100."
+    
+    computer_number = random.randint(1, 100)
+
+    if user_number == computer_number:
+        return f"Congratulations! the both numbers are {user_number}"
+    else:
+        return f"Your number was {user_number}, the computer's number was {computer_number}. Better luck next time!"
+    print(campare_numbers(50))
+    
+
