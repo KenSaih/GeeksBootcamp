562c8530d9b4d69053df5bfd6077b2b10da23d7e
KenSaih <saihkanza63@gmail.com>
2025-05-23 11:41:07 +0100

    Day3Ex4

---
+++ Week3/Day3/Exercises/Exercise4.js
@@ -0,0 +1,51 @@
+//Vacations Costs
+
+// function hotelCost(nights) {
+//     let nights = prompt("how many nignhts do you want book?");
+//     let costs = 140;
+//     let totalCosts = nights * costs;
+//     return totalCosts;
+// }
+
+function hotelCost(nights) {
+    return nights * 140; 
+}
+
+function planeRideCost(destination) {
+    if (destination.toLowerCase() === "london") return 183; 
+    if (destination.toLowerCase() === "paris") return 220;
+    return 300;
+}
+
+function rentalCarCost(days) {
+    let cost = days * 40;
+    if (days > 10) cost *= 0.95; // 5% discount
+    return cost;
+}
+
+function totalVacationCost() {
+    let nights = NaN;
+    let days = NaN;
+    let destination = "";
+    
+    while (isNaN(nights)) {
+        nights = Number(prompt("How many nights in the hotel?")); 
+    }
+    
+    while (!destination || typeof destination !== "string") {
+        destination = prompt("What's your destination?"); 
+    }
+    
+    while (isNaN(days)) {
+        days = Number(prompt("How many days for car rental?")); 
+    }
+    
+    const hotel = hotelCost(nights);
+    const plane = planeRideCost(destination);
+    const car = rentalCarCost(days);
+    
+    return `The car cost: $${car}, the hotel cost: $${hotel}, the plane tickets cost: $${plane}. 
+    Total: $${hotel + plane + car}`;
+}
+
+console.log(totalVacationCost()); 
